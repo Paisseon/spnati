a91d500773503c77d56f172cf688ceb3df8e2336
릴리 <68789766+Paisseon@users.noreply.github.com>
2020-09-27 15:29:14 -0500

    App v2.1
    Fixes this one weird NSException error and assigns value offlineView to Offline View in storyboard

---
+++ app/spnati/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Strip Poker Night at the Inventory
+//
+//  Created by Paisseon on 26/09/2020.
+//  Copyleft © 2020 Paisseon. All wrongs reversed.
+//
+
+import UIKit
+
+let appTitle = "SPNatI"
+let spnatiServer = URL(string: "https://spnati.faraway-vision.io")
+let allowedOrigin = "spnati.faraway-vision.io"
+let useUserAgentPostfix = true
+let userAgentPostfix = "iOSApp"
+let bouncyAndJiggly = true
+let changeAppTitleToPageTitle = false
+let useLightStatusBarStyle = false
+let progressBarColor = UIColor.white
+let offlineIconColor = UIColor.darkGray
+let buttonColor = UIColor.black
+let activityIndicatorColor = UIColor.white
