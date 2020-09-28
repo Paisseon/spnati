cad7684e08bededd03aeb906eccd26212012fdec
Paisseon <68789766+Paisseon@users.noreply.github.com>
2020-09-28 12:23:15 -0500

    Open source iOS app
    Adding the source code

---
+++ app/spnati/AppDelegate.swift
@@ -0,0 +1,38 @@
+//
+//  AppDelegate.swift
+//  Strip Poker Night at the Inventory
+//
+//  Created by Paisseon on 26/09/2020.
+//  Copyleft © 2020 Paisseon. All wrongs reversed.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
+
+}
+
